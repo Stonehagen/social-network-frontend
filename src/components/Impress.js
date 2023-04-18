d46e652bb185bf862e6ba6f03faf6c1f5ab28b49
Stonehagen <steinhagen@mailbox.org>
2023-04-18 22:58:43 +0200

    add impressum footer component

---
+++ src/components/Impress.js
@@ -0,0 +1,12 @@
+import '../styles/Impress.css';
+
+const Impress = () => {
+
+return (
+  <div className="Impress">
+    <p>Made with <span>❤</span> by <a href='https://stonehagen.com'>Tobias Steinhagen</a></p>
+  </div>
+);
+};
+
+export default Impress;
