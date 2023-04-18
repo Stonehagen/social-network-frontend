268c4511279250ad5df9ceb008dc6a8958ca73d4
Stonehagen <steinhagen@mailbox.org>
2023-04-18 23:41:00 +0200

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+const Header = ({ user, logout }) => {
+  return (
+    <div className="Header">
+      <h1>SOCIAL NETWORK</h1>
+    </div>
+  );
+};
+
+export default Header;
