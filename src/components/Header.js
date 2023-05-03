9af8b5aa702772f225b2aa9eb33011a4e8bb21bc
Stonehagen <steinhagen@mailbox.org>
2023-05-03 09:32:38 +0200

    add eslint diable comment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import '../styles/Header.css';
 import CornLogo from '../img/corn.svg';
--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,7 @@ const Home = ({ user, profile }) => {
     if (!user) {
       navigate('/login');
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (!profile) {
