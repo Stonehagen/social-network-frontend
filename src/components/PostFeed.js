f1a5aafc5c91c0037d56a11dd76c94cbd95d675f
Stonehagen <steinhagen@mailbox.org>
2023-05-04 09:56:46 +0200

    removed unused imports

--- src/components/PostFeed.js
+++ src/components/PostFeed.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import '../styles/PostFeed.css';
 import Post from './Post';
 
